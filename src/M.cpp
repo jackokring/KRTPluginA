82a1d8313de99e80b40c4e086d0a9a376e19b35c
jackokring <jackokring@gmail.com>
2021-08-16 16:48:11 +0100

    test 1

--- src/M.cpp
+++ src/M.cpp
@@ -4,14 +4,14 @@
 struct M : Module {
 	enum ParamIds {
 		LOW,
+		LGAIN,
 		HIGH,
-		CNTR,
-		TOP,
+		HGAIN,
 		NUM_PARAMS
 	};
 	enum InputIds {
-		ICTR,
-		ITOP,
+		ILOW,
+		IHIGH,
 		IN,
 		RTN,
 		NUM_INPUTS
@@ -24,7 +24,6 @@ struct M : Module {
 	enum LightIds {
 		NUM_LIGHTS
 	};
-
 
 	int maxPoly() {
 		int poly = 1;
@@ -40,7 +39,7 @@ struct M : Module {
 
 	/* 1P H(s) = 1 / (s + fb) */
     //ONE POLE FILTER
-	float f1, f2, b[PORT_MAX_CHANNELS][7];
+	float f1, f2, b[PORT_MAX_CHANNELS][8];
 
 	void setFK1(float fc, float fs) {//fb feedback not k*s denominator
 		//f1   = tanf(M_PI * fc / fs);
@@ -56,10 +55,10 @@ struct M : Module {
 
 	M() {
 		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
-		configParam(LOW, -2.f, 2.f, 0.f, "Low Frequency", " Oct (rel 50 Hz)");
-		configParam(HIGH, -2.f, 2.f, 0.f, "High Frequency", " Oct (rel 500 Hz");
-		configParam(CNTR, -2.f, 2.f, 0.f, "Center Master Frequency", " Oct (rel 1 kHz)");
-		configParam(TOP, -2.f, 2.f, 0.f, "Top Frequency", " Oct (rel 2.122 kHz");
+		configParam(LOW, -3.f, 4.f, 0.f, "Low Frequency", " Oct (rel 50 Hz)");//800
+		configParam(LGAIN, -20.f, 20.f, 20.f, "Low Gain", " dB");
+		configParam(HIGH, -4.f, 0.f, 0.f, "High Frequency", " Oct (rel 21.22 kHz");
+		configParam(HGAIN, -20.f, 20.f, -20.f, "High Gain", " dB");
 		//21kHz break on high boost
 	}
 
@@ -68,14 +67,29 @@ struct M : Module {
         return powf(2.f, val) * centre;
     }
 
+	float dB(float val) {
+        return powf(10.f, val * 0.05f);//dB scaling V??
+    }
+
+	float slopeGainL(float hz, float gain) {
+		//as 20dB per decade or 10 times
+		if(gain >= 1.f)	return hz * gain;
+		return hz / gain;
+	}
+
+	float slopeGainH(float hz, float gain) {
+		//as 20dB per decade or 10 times
+		if(gain >= 1.f)	return hz / gain;
+		return hz * gain;
+	}
+
 	void process(const ProcessArgs& args) override {
 		float fs = args.sampleRate;
 
-		float cent = params[CNTR].getValue();//a relative measure
-		float low = params[LOW].getValue() + cent;
-		float high = params[HIGH].getValue() + cent;
-		float top = params[TOP].getValue() + cent;
-		float chop = 21000.f;//top boost chop corner
+		float low = params[LOW].getValue();
+		float lgain = dB(params[LGAIN].getValue());
+		float high = params[HIGH].getValue();
+		float hgain = dB(params[HGAIN].getValue());
 		
 		int maxPort = maxPoly();
 #pragma GCC ivdep
@@ -83,37 +97,41 @@ struct M : Module {
 			float in = inputs[IN].getPolyVoltage(p);
 			float rtn = inputs[RTN].getPolyVoltage(p);
 
-			low = log(low + inputs[ICTR].getPolyVoltage(p), 50.f);//gain 10 = 1 decade
+			low = log(low + inputs[ILOW].getPolyVoltage(p), 50.f);//gain 10 = 1 decade
 			low = clamp(low, 0.f, fs * 0.5f);
-			high = log(high + inputs[ICTR].getPolyVoltage(p), 500.f);
+			high = log(high + inputs[IHIGH].getPolyVoltage(p), 21220.f);
 			high = clamp(high, 0.f, fs * 0.5f);
-			top = log(top + inputs[ITOP].getPolyVoltage(p), 2122.f);
-			top = clamp(top, 0.f, fs * 0.5f);
-
-			float decade = high / low;
-			float duck = chop / high;
+			
+			//extra constructed poles/zeros for actual
+			float lmid = slopeGainL(low, lgain);//decade
+			float hmid = slopeGainH(high, hgain);//decade
 
 			//forward "play" curve
-			setFK1(top, fs);//top cut
+			setFK1(hmid, fs);//top cut
 			float mid = process1(in, p, 0);
-			setFK1(high, fs);//low cut for add
-			mid -= process1(mid, p, 1);//HPF
+			setFK1(lmid, fs);//low cut
+			mid = process1(mid, p, 1);//HPF
+
 			setFK1(low, fs);
-			mid += process1(in, p, 2) * decade;//gained low
+			mid += process1(in, p, 2) * lgain;//gained low
+			setFK1(high, fs);
+			mid += (in - process1(in, p, 3)) * hgain;//gained high
+			float send = mid;
 
 			//reverse "record" curve
-			setFK1(top, fs);//top boost => cut for add
-			float mid2 = process1(rtn, p, 3);
-			setFK1(high, fs);//low cut for add
-			mid2 -= process1(mid2, p, 4);//HPF
+			setFK1(hmid, fs);//top cut
+			mid = process1(rtn, p, 4);
+			setFK1(lmid, fs);//low cut
+			mid = process1(mid, p, 5);//HPF
+
 			setFK1(low, fs);
-			mid2 += process1(rtn, p, 5) / decade;//dropped low
-			setFK1(chop, fs);//chopper high pass
-			mid2 += (mid2 - process1(rtn, p, 6)) * duck;//high shelf add 
+			mid += process1(rtn, p, 6) / lgain;//gained low
+			setFK1(high, fs);
+			mid += (rtn - process1(rtn, p, 7)) / hgain;//gained high
 
 			// OUTS
-			outputs[SEND].setVoltage(mid, p);
-			outputs[OUT].setVoltage(mid2, p);//inverse HP?
+			outputs[SEND].setVoltage(send, p);
+			outputs[OUT].setVoltage(mid, p);//inverse HP?
 		}
 	}
 };
@@ -147,12 +165,12 @@ struct MWidget : ModuleWidget {
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
 		addParam(createParamCentered<RoundBlackKnob>(loc(1, 1), module, M::LOW));
-		addParam(createParamCentered<RoundBlackKnob>(loc(1, 2), module, M::HIGH));
-		addParam(createParamCentered<RoundBlackKnob>(loc(1, 3), module, M::CNTR));
-		addParam(createParamCentered<RoundBlackKnob>(loc(1, 4), module, M::TOP));
+		addParam(createParamCentered<RoundBlackKnob>(loc(1, 2), module, M::LGAIN));
+		addParam(createParamCentered<RoundBlackKnob>(loc(1, 3), module, M::HIGH));
+		addParam(createParamCentered<RoundBlackKnob>(loc(1, 4), module, M::HGAIN));
 	
-		addInput(createInputCentered<PJ301MPort>(loc(0.75f, 5), module, M::ICTR));
-		addInput(createInputCentered<PJ301MPort>(loc(1.25f, 5), module, M::ITOP));
+		addInput(createInputCentered<PJ301MPort>(loc(0.75f, 5), module, M::ILOW));
+		addInput(createInputCentered<PJ301MPort>(loc(1.25f, 5), module, M::IHIGH));
 		addInput(createInputCentered<PJ301MPort>(loc(0.75f, 6), module, M::IN));
 		addInput(createInputCentered<PJ301MPort>(loc(1.25f, 6), module, M::RTN));
 
