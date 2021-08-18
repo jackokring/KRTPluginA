8c6e33253773f6d1c4de02c464bbf91d57702ce2
jackokring <jackokring@gmail.com>
2021-08-18 13:01:03 +0100

    io.

--- src/Q.cpp
+++ src/Q.cpp
@@ -12,9 +12,17 @@ struct Q : Module {
 		NUM_PARAMS
 	};
 	enum InputIds {
+		IN,
+		IOMEGA,//master uncertainty mass frequency
+		ISINGULAR_HYSTERISIS,//avoid div zero range
+		IPLANK,//quantum constant
+		INEWTON,//gravity constant
+		IMASS_RATIO,//mass ratio of driver to driven
+		IANGLE,//radial to tangential
 		NUM_INPUTS
 	};
 	enum OutputIds {
+		OUT,
 		NUM_OUTPUTS
 	};
 	enum LightIds {
@@ -29,6 +37,23 @@ struct Q : Module {
 	}
 };
 
+//geometry edit
+#define HP 5
+#define LANES 2
+#define RUNGS 7
+
+//ok
+#define HP_UNIT 5.08
+#define WIDTH (HP*HP_UNIT)
+#define X_SPLIT (WIDTH / 2.f / LANES)
+
+#define HEIGHT 128.5
+#define Y_MARGIN 0.05f
+#define R_HEIGHT (HEIGHT*(1-2*Y_MARGIN))
+#define Y_SPLIT (R_HEIGHT / 2.f / RUNGS)
+
+//placement macro
+#define loc(x,y) mm2px(Vec(X_SPLIT*(1+2*(x-1)), (HEIGHT*Y_MARGIN)+Y_SPLIT*(1+2*(y-1))))
 
 struct QWidget : ModuleWidget {
 	QWidget(Q* module) {
