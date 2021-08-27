711ce1483e4dadc9419311b8ae280de6db7d1ee2
jackokring <jackokring@gmail.com>
2021-08-27 12:50:21 +0100

    tests later

--- src/U.cpp
+++ src/U.cpp
@@ -1,5 +1,5 @@
 #include "plugin.hpp"
-
+#include <random>
 
 struct U : Module {
 	enum ParamIds {
@@ -21,11 +21,90 @@ struct U : Module {
 		NUM_LIGHTS
 	};
 
-	U() {
-		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
+	int maxPoly() {
+		int poly = 1;
+		for(int i = 0; i < NUM_INPUTS; i++) {
+			int chan = inputs[i].getChannels();
+			if(chan > poly) poly = chan;
+		}
+		for(int o = 0; o < NUM_OUTPUTS; o++) {
+			outputs[o].setChannels(poly);
+		}
+		return poly;
 	}
 
+	/* 1P H(s) = 1 / (s + fb) */
+    //ONE POLE FILTER
+	float f1, f2, b[PORT_MAX_CHANNELS * 3];
+
+	void setFK1(float fc, float fs) {//fb feedback not k*s denominator
+		//f1   = tanf(M_PI * fc / fs);
+		f1	 = tanpif(fc / fs);
+		f2   = 1 / (1 + f1);
+	}
+
+	float process1(float in, int p) {
+		float out = (f1 * in + b[p]) * f2;
+		b[p] = f1 * (in - out) + out;
+		return out;//lpf default
+	}
+
+	U() {
+		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
+		configParam(QUANTIZE, 1.f, 12.f, 1.f, "Interval", " Semitones");
+		configParam(NOISE, -8.f, 8.f, 0.f, "Noise Filter", " Oct");
+		for(int i = 0; i < 3; i++) {
+			sampled[i] = 0.f;
+		}
+	}
+
+	dsp::SchmittTrigger sh[3];
+	float sampled[3];
+
+	float moduloD(float x, float m) {
+		float div = x / m;
+		long d = (long) div;
+		float rem = d * m;
+		return rem;
+	}
+
+	//obtain mapped control value
+    float log(float val, float centre) {
+        return powf(2.f, val) * centre;
+    }
+
+	static std::default_random_engine gen;
+
 	void process(const ProcessArgs& args) override {
+		float fs = args.sampleRate;
+		int maxPort = maxPoly();
+
+		float q = params[QUANTIZE].getValue() / 12.f;//semitone volts
+		float n = log(params[NOISE].getValue(), dsp::FREQ_C4);
+		n = clamp(n, 0.f, fs * 0.5f);//filter limit
+		std::normal_distribution<float> dist(0, 5);
+
+#pragma GCC ivdep
+		for(int p = 0; p < maxPort; p++) {
+
+#pragma GCC ivdep
+			for(int i = 0; i < 3; i++) {
+				float in = 0.f;
+				if(inputs[IN + i].isConnected()) {
+					in = inputs[IN + i].getPolyVoltage(p);
+				} else {
+					in = dist(gen);//noise
+					setFK1(n, fs);
+					in = process1(in, p + i * PORT_MAX_CHANNELS);
+				}
+				float clk = inputs[CLK + i].getPolyVoltage(p);
+				bool trig = sh[i].process(rescale(clk, 0.1f, 2.f, 0.f, 1.f));
+				if(trig) sampled[i] = in;//sh
+				outputs[OUT + i].setVoltage(sampled[i], p);//out
+				float quant = moduloD(sampled[i] - (q * 0.5f), q);//off by
+				outputs[QOUT + i].setVoltage(quant, p);
+			}
+		}
 	}
 };
 
