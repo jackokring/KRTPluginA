1c3d78e1fe54f97bb4c4e9e68cb4bf6164d6080a
jackokring <jackokring@gmail.com>
2021-09-09 21:22:26 +0100

    gfx

--- src/I.cpp
+++ src/I.cpp
@@ -8,7 +8,7 @@ struct I : Module {
 		NUM_PARAMS
 	};
 	enum InputIds {
-		CLK.
+		CLK,
 		RST,
 		NUM_INPUTS
 	};
@@ -31,6 +31,23 @@ struct I : Module {
 	}
 };
 
+//geometry edit
+#define HP 5
+#define LANES 2
+#define RUNGS 7
+
+//ok
+#define HP_UNIT 5.08
+#define WIDTH (HP*HP_UNIT)
+#define X_SPLIT (WIDTH / 2.f / LANES)
+
+#define HEIGHT 128.5
+#define Y_MARGIN 0.05f
+#define R_HEIGHT (HEIGHT*(1-2*Y_MARGIN))
+#define Y_SPLIT (R_HEIGHT / 2.f / RUNGS)
+
+//placement macro
+#define loc(x,y) mm2px(Vec(X_SPLIT*(1+2*(x-1)), (HEIGHT*Y_MARGIN)+Y_SPLIT*(1+2*(y-1))))
 
 struct IWidget : ModuleWidget {
 	IWidget(I* module) {
@@ -41,6 +58,18 @@ struct IWidget : ModuleWidget {
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+
+		for(int i = 0; i < 3; i++) {
+			addParam(createParamCentered<RoundBlackKnob>(loc(1, i + 1), module, I::DIV + i));
+			addParam(createParamCentered<RoundBlackKnob>(loc(2, i + 1), module, I::PHA + i));
+			addOutput(createOutputCentered<PJ301MPort>(loc(2, i + 4), module, I::OUT + i));
+		}
+
+		addInput(createInputCentered<PJ301MPort>(loc(1, 4), module, I::CLK));
+		addInput(createInputCentered<PJ301MPort>(loc(1, 5), module, I::RST));
+		addOutput(createOutputCentered<PJ301MPort>(loc(1, 6), module, I::DWN));
+		addOutput(createOutputCentered<PJ301MPort>(loc(1, 7), module, I::SANS));
+		addOutput(createOutputCentered<PJ301MPort>(loc(2, 7), module, I::XOR));
 	}
 };
 
